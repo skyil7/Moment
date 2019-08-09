2ac674e64bb29c4d765ef84cd3632ed697e7bf2c
SongJi-Won <s26788761@naver.com>
2019-08-10 02:09:13 +0900

    지원 mainpage.html  지도 추가

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from board.models import Post, Category, Tag
+from Moment import jiwon_self
 
 def home(request):
     if request.user.is_authenticated:#로그인 됨
@@ -14,4 +15,5 @@ def mainList(request):
     posts = Post.objects.all()
     categories = Category.objects.all()
     tags = Tag.objects.all()
-    return render(request, 'mainpage.html', {'posts':posts, 'categories':categories, 'tags':tags})
+    client_id = jiwon_self.client_id
+    return render(request, 'mainpage.html', {'posts':posts, 'categories':categories, 'tags':tags, 'client_id':client_id})
