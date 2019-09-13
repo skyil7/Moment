a91c601f39ce52229bea765eb8ebf08b7efad9e0
GioPaikHome <giopaik@naver.com>
2019-09-13 18:23:25 +0900

    프로젝트 갈아엎음 ㅎ

---
+++ DjangoBackend/post/serializer.py
@@ -0,0 +1,9 @@
+from .models import Post
+from rest_framework import serializers
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        # fields = '__all__'
+        fields = ['pub_date', 'text']
+        read_only_fields = ('pub_date',)
---
+++ README.md
@@ -0,0 +1,7 @@
+# API 설명
+## post
+- pub_date
+    - Readonly
+    - AutoNow
+- text
+    - 텍스트 내용
