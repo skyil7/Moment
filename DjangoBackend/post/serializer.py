c0d03b5b589bd8242661ee44c5cc18f1704bccd7
skyil7 <giopaik@naver.com>
2019-09-19 22:59:26 +0900

    Post model 수정

--- DjangoBackend/post/serializer.py
+++ DjangoBackend/post/serializer.py
@@ -5,5 +5,5 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         # fields = '__all__'
-        fields = ['pub_date', 'text', 'lat', 'lng']
+        fields = ['pub_date', 'title', 'text', 'lat', 'lng', 'img']
         read_only_fields = ('pub_date',)
